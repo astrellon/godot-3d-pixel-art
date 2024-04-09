7f39ba706b2fdd53198ef1fd2c5828633ecf355e
Astrellon <sovereign250@gmail.com>
2024-04-09 19:32:21 +1000

    Added dogica pixel font. Attempting to update the navmesh

--- game.gd
+++ game.gd
@@ -5,10 +5,16 @@ extends Control
 @export var render_size: TextureRect = null
 @export var viewport: SubViewport = null
 @export var pixel_size := 2.0
+@export var building_file: PackedScene
 
 signal input_event(event: InputEvent)
 
+
+enum ClickMode { None, Build }
+
+
 var _clicked_at := Vector2.ZERO
+var _click_mode := ClickMode.None
 var _clicked := false
 var _window_size := Vector2(1280, 720)
 
@@ -29,9 +35,10 @@ func _on_window_resize() -> void:
 
 func _input(event: InputEvent):
 	input_event.emit(event)
-	if event is InputEventMouseButton && event.button_index == 1 && event.pressed:
+	if event is InputEventMouseButton && event.button_index == 1 && event.is_released():
 		_clicked_at = event.position * _window_size / render_size.size / pixel_size
 		_clicked = true
+
 
 func _physics_process(delta) -> void:
 	if !_clicked:
@@ -48,19 +55,41 @@ func _physics_process(delta) -> void:
 	var result = space_state.intersect_ray(ray_cast)
 
 	if result:
-		var hit = result['collider']
-		var changed_target := false
-		if hit:
-			var parent = hit.get_parent()
-			if parent is Character:
-				_change_target(parent)
-				changed_target = true
-		
-		if !changed_target && target:
-			var hit_position = result['position']
-			target.calculate_nav(hit_position)
+		if _click_mode == ClickMode.Build:
+			_handle_click_build(result)
+		elif _click_mode == ClickMode.None:
+			_handle_click_general(result)
 	else:
 		print("No raycast hit")
+
+func _handle_click_build(hit_result) -> void:
+	_click_mode = ClickMode.None
+	var hit_position = hit_result['position']
+	var new_building = building_file.instantiate()
+	if new_building is Node3D:
+		new_building.position = hit_position
+		
+	add_child(new_building)
+	
+	#var nav_region = find_child("NavigationRegion3D")
+	#if nav_region is NavigationRegion3D:
+		#print("Updating nav region")
+		#nav_region.bake_navigation_mesh()
+	#else:
+		#print("Could not find nav region!")
+
+func _handle_click_general(hit_result) -> void:
+	var hit = hit_result['collider']
+	var changed_target := false
+	if hit:
+		var parent = hit.get_parent()
+		if parent is Character:
+			_change_target(parent)
+			changed_target = true
+	
+	if !changed_target && target:
+		var hit_position = hit_result['position']
+		target.calculate_nav(hit_position)
 
 
 func _change_target(new_target: Character) -> void:
@@ -71,3 +100,8 @@ func _change_target(new_target: Character) -> void:
 		new_target.is_selected = true
 	
 	target = new_target
+
+
+func _on_build_click() -> void:
+	_clicked = false
+	_click_mode = ClickMode.Build
